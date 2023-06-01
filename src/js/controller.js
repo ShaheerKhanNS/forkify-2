17a160512df510965a1abee56cc5bebf43ec6a96
Shaheer Khan NS <khanshaheer43@gmail.com>
2023-06-01 17:43:29 +0530

    Added MVC Pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,35 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
+    recipeView.renderSpinner();
+    // 1) Loading Recipe
+
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
+
+    // 2) Rendering
+
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
